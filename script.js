2edf8e18dd947cb995d9ec2baff110fa317cd7a6
dpinkvilla <117904994+dpinkvilla@users.noreply.github.com>
2024-02-19 17:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function fun() {
+			let fname=document.getElementById("nik").value;
+	        let lname=document.getElementById("nik1").value;
+	         let phone=document.getElementById("nik2").value;
+	        let mail=document.getElementById("nik3").value;
+
+	  let alertText = "First Name: " +fname + "\n" +
+		                "last Name: " + lname + "\n" +
+		                "Phone Number: " + phone+"\n"+
+                        "Email: " + mail;
+                        
+	   alert(alertText);
+		}
